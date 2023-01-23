350f060b9010753520ad9d5c230c0c6553b516b3
Huntski <wieberan@hotmail.nl>
2023-01-23 13:49:50 +0100

    Feature: Create functionable bubbles in gallery

---
+++ js/modules/bubbles.js
@@ -0,0 +1,184 @@
+export class BubbleInterface {
+    rows = []
+
+    constructor(containerId = 'gallery', bubbles = []) {
+        this.gallery = document.getElementById(containerId)
+
+        console.log(this.gallery)
+
+        this.createBubbleElementsWithRowsAndColumns(bubbles)
+        this.createMovableGallery(this.gallery)
+    }
+
+    createBubbleElementsWithRowsAndColumns(bubbles) {
+        const bubblesLimitPerRow = 7
+
+        for (const key in bubbles) {
+            let rowToAddBubbleTo = null
+
+            if (! this.rows.length) {
+                this.createRow([bubbles[key]])
+
+                continue
+            }
+
+            for (let a = 0; a < this.rows.length; a++) {
+                if (this.rows[a].bubblesAmount > bubblesLimitPerRow && this.rows[a+1] === undefined) {
+                    this.createRow([bubbles[key]])
+                }
+
+                if (this.rows[a].bubblesAmount > bubblesLimitPerRow) {
+                    continue
+                }
+
+                rowToAddBubbleTo = this.rows[a]
+                break
+            }
+
+            this.addBubble(rowToAddBubbleTo, this.createBubble(bubbles[key]))
+        }
+    }
+
+    addBubble(row, element) {
+        row.element.append(element)
+        row.bubblesAmount++
+    }
+
+    createBubble(bubble = null) {
+        const element = document.createElement('div')
+        element.classList.add('bubble')
+        if (bubble) {
+            element.style.backgroundImage = `url(${bubble.pictures[0]})`
+        }
+
+        return element
+    }
+
+    createRow(bubbles) {
+        // Create row element.
+        const rowElement = document.createElement('div')
+        rowElement.classList.add('row')
+
+        // Create row object to row list.
+        const row = {
+            element: rowElement,
+            bubblesAmount: 0
+        }
+
+        // Add bubbles to new row element.
+        if (bubbles) {
+            bubbles.forEach(bubble => {
+                const bubbleElement = this.createBubble(bubble)
+
+                this.addBubble(row, bubbleElement)
+            })
+        }
+
+        // Add row element to gallery.
+        this.gallery.appendChild(rowElement)
+
+        this.rows.push(row)
+    }
+
+    get centerBubble() {
+        const centerRow = this.rows[Math.round((this.rows.length - 1) / 2)]
+
+        const bubbles = centerRow.element.getElementsByClassName('bubble')
+
+        return bubbles[Math.round((bubbles.length - 1) / 2)]
+    }
+
+    createMovableGallery() {
+        const gallery = this.gallery
+
+        let xMouseDownAt = 0
+        let yMouseDownAt = 0
+
+        let xPosition = 0
+        let yPosition = 0
+
+        let moveX
+        let moveY
+
+        let mouseDown
+
+        gallery.onmousedown = e => {
+            xMouseDownAt = e.clientX
+            yMouseDownAt = e.clientY
+            mouseDown = true
+        }
+
+        gallery.onmouseup = e => {
+            mouseDown = false
+            xPosition = moveX
+            yPosition = moveY
+        }
+
+        gallery.onmousemove = e => {
+            // Check if mouse is pressed down.
+            if (!mouseDown) return
+
+            const xDistance = e.clientX - xMouseDownAt
+            const yDistance = e.clientY - yMouseDownAt
+
+            moveX = xPosition + xDistance
+            moveY = yPosition + yDistance
+
+            // scaleBubblesWithSideOffset()
+
+            gallery.animate({
+                transform: `translate(${moveX}px, ${moveY}px)`
+            }, {
+                duration: 300,
+                fill: 'forwards'
+            })
+        }
+    }
+}
+
+function scaleBubblesWithSideOffset() {
+    const bubbles = document.querySelectorAll('.bubble')
+
+    for (let i = 0; i < bubbles.length; i++) {
+        scaleElement(bubbles[i])
+    }
+}
+
+function scaleElement(element) {
+    const rect = element.getBoundingClientRect()
+
+    let percentage = 100
+    const gap = 30
+
+    if (rect.left < 50) {
+        percentage = 100 / (rect.width+gap) * (rect.width + rect.left)
+
+        if (percentage > 100) {
+            percentage = 100
+        }
+
+        if (percentage < 70) {
+            percentage = 70
+        }
+    }
+
+    if (rect.right < 50) {
+        percentage = 100 / (rect.width+gap) * (rect.width + rect.right)
+
+        if (percentage > 100) {
+            percentage = 100
+        }
+
+        if (percentage < 70) {
+            percentage = 70
+        }
+    }
+
+    element.animate({
+        transform: `scale(${percentage}%)`
+    }, {
+        fill: 'forwards'
+    })
+
+    return percentage
+}
---
+++ js/shops.js
@@ -0,0 +1,70 @@
+const shops = [
+    {
+        name: 'The Besties',
+        pictures: [
+            'https://images.blue-tomato.com/is/image/bluetomato/amsterdam-sep-19-gallery-image-520x390?$b36$'
+        ]
+    },
+    {
+        name: 'Nameless',
+        pictures: [
+            'https://images.ctfassets.net/biom0eqyyi6b/1zcsUu3QK84aQ60kKE04um/4089304479a7755813e0c51ea1838a4c/Toys_02.jpg?fit=fill&w=450&h=450',
+        ]
+    },
+    {
+        name: 'Coffee tent',
+        pictures: [
+            'https://dutchreview.com/wp-content/uploads/tourist-shop-amsterdam-scaled.jpg',
+        ]
+    },
+    {
+        name: 'Lab',
+        pictures: [
+            'https://mytravelingcam.com/wp-content/uploads/2022/03/IMG_20220329_194635-scaled.jpg',
+        ]
+    },
+    {
+        name: 'Pathway',
+        pictures: [
+            'https://dxkmbl8uwuv9p.cloudfront.net/moscot/1605061390367/efe9596a-63e6-434e-98ea-1ccb6d8a7475/1.jpg',
+        ]
+    },
+    {
+        name: 'French revolution',
+        pictures: [
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhHZ57cesWdnqJmBm-tMOcl9p_EkomA8iayw&usqp=CAU',
+        ]
+    },
+    {
+        name: 'Travel companion',
+        pictures: [
+            'https://www.traveloffpath.com/wp-content/uploads/2022/04/amsterdam-coffee-shop-canal.jpg.webp',
+        ]
+    },
+    {
+        name: 'Travel companion',
+        pictures: [
+            'https://media.timeout.com/images/105302793/image.jpg',
+        ]
+    },
+    {
+        name: 'Revolution',
+        pictures: [
+            'https://media.timeout.com/images/105302793/image.jpg',
+        ]
+    },
+    {
+        name: 'Amsterdam hotline',
+        pictures: [
+            'https://www.thetrainline.com/cms/media/4171/netherlands_amsterdam_shopping.jpg?mode=crop&width=1080&height=1080&quality=70',
+        ]
+    },
+    {
+        name: 'Hotpot',
+        pictures: [
+            'https://media.cntraveler.com/photos/5b033123a227d435569629af/master/pass/Athenaeum-Boekhandel-&-Nieuwscentrum__2018_CONA6.jpg',
+        ]
+    },
+]
+
+export default shops
