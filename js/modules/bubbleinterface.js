668de1348e5b2987771708dcb65e0d6ef96a0dc8
Huntski <wieberan@hotmail.nl>
2023-01-27 01:13:07 +0100

    Feature: Add working shop popup

--- js/modules/bubbleinterface.js
+++ js/modules/bubbleinterface.js
@@ -61,7 +61,42 @@ export default class BubbleInterface {
         element.classList.add('bubble')
 
         if (typeof bubble === 'object') {
-            element.style.backgroundImage = `url(${bubble.pictures[0]})`
+            const shop_picture = './img/shops/' + bubble.pictures[0]
+
+            element.innerHTML = `
+                <div class="bubble-popup">
+                    <img class="bubble-popup__picture" src="${shop_picture}" alt="Very cute cat.">
+
+                    <div class="bubble-popup__actions">
+                        <h2>${bubble.name}</h2>
+                    
+                        <button class="like-button">
+                            <span>30</span>
+                        
+                            <svg class="icon" viewBox="0 0 175 159" fill="#F54949" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M157.91 87.7452L87.5504 158.19L87.5 158.14L87.4496 158.19L17.0899 87.7452C-32.8043 37.7908 37.5555 -32.654 87.4496 17.3005L87.5 17.2501L87.5504 17.3005C137.445 -32.654 207.804 37.7908 157.91 87.7452Z"/>
+                            </svg>
+                        </button>
+                    </div>
+                    
+                    <div class="bubble-popup__types">
+                        <span>Japanese</span>
+                        <span>Korea</span>
+                    </div>
+                    
+                    <div class="bubble-popup__info">
+                        <p>Stadsplein 95, 1181 ZM Amstelveen</p>
+                        <p><span>Telefoon:</span> 020 312 1237</p>
+                    </div>
+              
+                    
+                    <button class="bubble-popup__button">
+                        <span>START</span>
+                    </button>
+                </div>   
+            `
+
+            element.style.backgroundImage = `url(${shop_picture})`
         }
 
         /**
@@ -77,8 +112,8 @@ export default class BubbleInterface {
                     <button class="like-button">
                         <span>12</span>
                     
-                        <svg class="icon" viewBox="0 0 175 159" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M157.91 87.7452L87.5504 158.19L87.5 158.14L87.4496 158.19L17.0899 87.7452C-32.8043 37.7908 37.5555 -32.654 87.4496 17.3005L87.5 17.2501L87.5504 17.3005C137.445 -32.654 207.804 37.7908 157.91 87.7452Z" fill="white"/>
+                        <svg class="icon" viewBox="0 0 175 159" fill="#F54949" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M157.91 87.7452L87.5504 158.19L87.5 158.14L87.4496 158.19L17.0899 87.7452C-32.8043 37.7908 37.5555 -32.654 87.4496 17.3005L87.5 17.2501L87.5504 17.3005C137.445 -32.654 207.804 37.7908 157.91 87.7452Z"/>
                         </svg>
                     </button>
                
